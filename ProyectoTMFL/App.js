6d38b10dd78b7a47efe5eb337f6c3230615cf35c
francoleone23 <fleone@udesa.edu.ar>
2022-06-16 15:41:42 -0300

    EL BICHO SU

--- ProyectoTMFL/App.js
+++ ProyectoTMFL/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import MainNavigation from './src/Components/MainNavigation';
-import Login from './src/screens/Login';
 
 
 export default function App() {
--- ProyectoTMFL/src/Screens/Register.js
+++ ProyectoTMFL/src/Screens/Register.js
@@ -17,4 +17,73 @@ class Register extends Component{
             userName:''
         }
     }
+
+    render(){
+        console.log(this.state.email);
+        console.log(this.state.password);
+        console.log(this.props);
+        return(
+            <View style={styles.container}>
+                <Text style={styles.title}>Registro</Text>
+                <TextInput 
+                    style={styles.field}
+                    keyboardType='default'
+                    placeholder='Email'
+                    onChangeText={text => this.setState({ email: text})}
+                />
+                <TextInput 
+                    style={styles.field}
+                    keyboardType='default'
+                    placeholder='User Name'
+                    onChangeText={text => this.setState({ userName: text})}
+                />
+                <TextInput 
+                    style={styles.field}
+                    keyboardType='default'
+                    placeholder='password'
+                    secureTextEntry={true}
+                    onChangeText={text => this.setState({ password: text})}
+                />
+                <TouchableOpacity style={styles.button} onPress={()=>this.props.route.params.register(this.state.email, this.state.password, this.state.userName)}>
+                    <Text style={styles.buttonText}>Registrarme</Text>
+                </TouchableOpacity>   
+
+                <Text> El error es: {this.props.errores} </Text>
+                 <TouchableOpacity onPress={ ()=>this.props.navigation.navigate('Login') }>
+                        <Text>Ya tengo cuenta</Text>
+                 </TouchableOpacity>
+            
+            </View>
+
+        )
+    }
+
 }
+
+const styles = StyleSheet.create({
+    container:{
+        paddingHorizontal:10,
+        marginTop: 10
+    },
+    title:{
+        marginBottom:20
+    },
+    field:{
+        borderColor: '#dcdcdc',
+        borderWidth: 1,
+        borderRadius: 2,
+        padding:3,
+        marginBottom:8
+    },
+    button: {
+        borderRadius: 2,
+        padding:3,
+        backgroundColor: 'green',
+    },
+    buttonText:{
+        color: '#fff'
+    }
+})
+
+export default Register;
+
